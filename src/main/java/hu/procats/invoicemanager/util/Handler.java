6e218bb128109429be34f9f8268e2efc1ccdb807
aryx <aryx@pvga.hu>
2021-07-20 09:06:56 +0200

    validációs hibákhoz globális exception handler

--- src/main/java/hu/procats/invoicemanager/util/Handler.java
+++ src/main/java/hu/procats/invoicemanager/util/Handler.java
@@ -1,6 +1,7 @@
 package hu.procats.invoicemanager.util;
 
 import hu.procats.invoicemanager.models.FrontendExceptionDetails;
+import hu.procats.invoicemanager.models.ValidationExceptionDetails;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class Handler
@@ -18,6 +23,22 @@ public class Handler
             Exception ex, WebRequest request) {
         return new ResponseEntity<Object>(
                 new FrontendExceptionDetails(ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ ConstraintViolationException.class })
+    public ResponseEntity<Object> handleValidationException(
+            ConstraintViolationException ex, WebRequest request) {
+
+        List<String> errors = new ArrayList<>();
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            errors.add(violation.getMessage());
+        }
+
+        return new ResponseEntity<Object>(
+                new ValidationExceptionDetails("Adatbeviteli hiba.", errors),
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST
+        );
     }
 }
 
